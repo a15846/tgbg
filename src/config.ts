479e9ae63e39a84c11ad1665e169852680f68561
a15846 <hmgongg@gmail.com>
2025-08-03 08:15:41 +0800

    Add files via upload

---
+++ src/config.ts
@@ -0,0 +1,11 @@
+// src/config.ts
+// 针对博主特定的数据
+export const ADMIN_USER = {
+  nickname: "ddg",
+  email: "djjd15846@gmail.com",
+  website: "https://ddgdd.232132.xyz",
+  avatar: "https://ddgdd.232132.xyz/ddgdd.webp",
+};
+
+// 你不希望用户使用的名称与邮箱
+export const SENSITIVE_USERS = ["ddg", "DDG",  "admin", "博主", "djjd15846@gmail.com"];
